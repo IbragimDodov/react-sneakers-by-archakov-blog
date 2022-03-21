f90598ff5eb8c4f7280a1e739a22af01f661117c
IbragimDodov <utisey@gmail.com>
2022-03-21 21:19:09 +0300

    #3 complited

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 
-function Cart() {
+import styles from './Cart.module.scss';
+
+function Cart(props) {
   return (
-    <div className="card">
-      <div className="favorite">
+    <div className={styles.card}>
+      <div className={styles.favorite}>
         <img src="/img/heart-unliked.svg" alt="unliked-icon" />
       </div>
-      <img width={133} height={112} src="/img/sneakers/1.jpg" alt="sneakers-photo" />
-      <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-      <div className="cardBottom">
+      <img width={133} height={112} src={props.imageUrl} alt="sneakers-photo" />
+      <h5>{props.title}</h5>
+      <div className={styles.cardBottom}>
         <div cardBottom__inner>
           <span>Цена:</span>
-          <b>12 999 руб.</b>
+          <b>{props.price} руб.</b>
         </div>
         <button className="button">
           <img width={11} height={11} src="/img/plus.svg" alt="plus-logo" />
